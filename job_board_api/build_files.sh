04bff45c434bad142617cd509e527353d1ac4832
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-01 22:08:44 +0200

    builder

--- job_board_api/build_files.sh
+++ job_board_api/build_files.sh
@@ -3,6 +3,7 @@ python3.12 -m pip install virtualenv
 python3.12 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+mkdir -p staticfiles
 python3.12 manage.py collectstatic --noinput
 
 
