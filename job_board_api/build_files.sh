9caab1326d08004854ce66a168d71eb874d413fb
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-01 21:04:29 +0200

    vuild files edit for vercel

--- job_board_api/build_files.sh
+++ job_board_api/build_files.sh
@@ -3,5 +3,9 @@
 #install dependences
 pip install -r requirements.txt
 
+# Create the staticfiles_build directory if it doesn't exist
+mkdir -p staticfiles_build
+
 # Collect static files
-python3.9 manage.py collectstatic
+python manage.py collectstatic --noinput
+
