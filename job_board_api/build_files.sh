94bd9b08395309199e65e79496ad12f92fbb9928
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-01 19:22:49 +0200

    build_files edit

--- job_board_api/build_files.sh
+++ job_board_api/build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Create the staticfiles_build directory if it doesn't exist
+mkdir -p staticfiles
+
+#install dependancies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
