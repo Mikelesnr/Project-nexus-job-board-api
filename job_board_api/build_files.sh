5ff51022120fa231e44ed42f3dbc6742a142aa24
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-01 21:44:55 +0200

    adding static files manually

--- job_board_api/build_files.sh
+++ job_board_api/build_files.sh
@@ -3,8 +3,4 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Create the staticfiles_build directory if it doesn't exist
-mkdir -p staticfiles_build
 
-# Collect static files
-python manage.py collectstatic --noinput
