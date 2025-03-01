81b84e6a28dd6966996ee003e0bd394809ae66ad
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-01 22:11:25 +0200

    staticfiles

--- job_board_api/build_files.sh
+++ job_board_api/build_files.sh
@@ -3,6 +3,7 @@ python3.12 -m pip install virtualenv
 python3.12 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+rm-rf staticfiles
 mkdir -p staticfiles
 python3.12 manage.py collectstatic --noinput
 
