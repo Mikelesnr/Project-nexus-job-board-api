fd614e3a1d990c753042f3f4a196885138448284
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-01 21:37:07 +0200

    build

--- job_board_api/build_files.sh
+++ job_board_api/build_files.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Exit on errors
-set -e
-
-# Create and activate the virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
 # Install dependencies
 pip install -r requirements.txt
 
