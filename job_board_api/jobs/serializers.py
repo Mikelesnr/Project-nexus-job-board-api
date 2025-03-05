a9ed3156389bc8ba0066e9ecf703ab3366d62f09
Michael Mwanza <mwanza.n.m@gmail.com>
2025-03-05 02:53:26 +0200

    making username readonly

--- job_board_api/jobs/serializers.py
+++ job_board_api/jobs/serializers.py
@@ -31,3 +31,4 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+        read_only_fields = ['username']
